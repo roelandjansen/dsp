54b68f4ffa59443adf5f162b9355a2f4dc572217
Roeland Jansen <roeland@newlaptop.invalid>
2025-06-02 11:48:13 +0200

    removed stray file

--- Signal_Mean/main.c
+++ Signal_Mean/main.c
@@ -5,8 +5,8 @@
 
 #define SIG_LENGTH 320
 
-extern double InputSignal_f32_1kHz_15kHz[SIG_LENGTH];
-double calc_signal_mean(double *sig_src_arr, int sig_length);
+extern double InputSignal_f32_1kHz_15kHz[int SIG_LENG];
+double calc_signal_mean(double *sig_src_array, int SIG_LENGTH);
 double MEAN;
 
 int main()
@@ -18,14 +18,14 @@ int main()
       return 0;
 }
 
-double calc_signal_mean(double *sig_src_arr,int sig_length)
+double calc_signal_mean(double *sig_src_array,int sig_length)
 
 {
     double _mean = 0.0;
-    inwt i;
+    int i;
         for (i = 0; i < sig_length ; i++)
             {
-                _mean = _mean + sig_src_arr[i];
+                _mean = _mean + sig_src_array[i];
          }
 
     _mean = _mean / (double) sig_length;
