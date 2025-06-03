d91f4320fc96b2a56b15de5dac7933e594f06b79
Roeland Jansen <roeland@newlaptop.invalid>
2025-06-03 07:53:28 +0200

    moved iterators in the scope of the loop(s)

--- Signalvariance/dsp_project/dsp_project/main.c
+++ Signalvariance/dsp_project/dsp_project/main.c
@@ -1,34 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-/* this takes an array of SIG_LENGTH] and returns the mean value */
+/* 20250601 this takes an array of SIG_LENGTH] and returns the mean value */
+/* 20250602 it also takes the same array and returns the variance */
 
 #define SIG_LENGTH 320
 
 extern double InputSignal_f32_1kHz_15kHz[SIG_LENGTH];
 double calc_signal_mean(double *sig_src_arr, int sig_length);
-double MEAN;
+double calc_signal_variance(double *signal_source_arr, double sig_mean, int sig_length);
 
-int main()
+double MEAN;
+double VARIANCE;
+
+int main(void)
 {
 
-      MEAN = calc_signal_mean (&InputSignal_f32_1kHz_15kHz[0], SIG_LENGTH);
-      printf("\n\nMEAN: %f\n\n\n", MEAN);
+      MEAN      = calc_signal_mean    (&InputSignal_f32_1kHz_15kHz[0],       SIG_LENGTH);
+      VARIANCE  = calc_signal_variance(&InputSignal_f32_1kHz_15kHz[0], MEAN, SIG_LENGTH);
+
+            printf("\n\nMEAN:     %f\n",   MEAN    );
+            printf("\n\nVARIANCE: %f\n\n", VARIANCE);
 
       return 0;
 }
 
 double calc_signal_mean(double *sig_src_arr,int sig_length)
-
 {
     double _mean = 0.0;
-    inwt i;
-        for (i = 0; i < sig_length ; i++)
-            {
-                _mean = _mean + sig_src_arr[i];
+
+        for (int i = 0; i < sig_length ; i++)   {
+            _mean = _mean + sig_src_arr[i];
          }
 
     _mean = _mean / (double) sig_length;
 
     return _mean;
+}
+
+
+double calc_signal_variance(double *signal_source_arr, double sig_mean, int sig_length)
+{
+    double _variance = 0.0;
+        for (int i = 0; i < sig_length; i++)    {
+            _variance = _variance + pow(signal_source_arr[i] - sig_mean, 2);
+         }
+
+        _variance = _variance / (sig_length - 1);
+
+        return _variance;
 }
